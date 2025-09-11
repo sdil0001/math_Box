ad2e3f7b232f387131bf64b4b62a238ffb390eb5
seo <seo@naver.com>
2025-09-11 19:02:57 +0900

    Revert "error scv"
    This reverts commit 0f87603e248296c3fd4f54e38864463cef8f7289.

--- calculator.py
+++ calculator.py
@@ -6,10 +6,17 @@ def subtract(a, b):
 	return a -b
 
 def multiply(a, b):
+<<<<<<< HEAD
 	return a * b
 
 def divide(a, b):
     return a / b
+<<<<<<< HEAD
 
 def square(a):
 	return a*a
+=======
+=======
+	return a * b
+>>>>>>> a8b117adf4e82990f1c2fc172bab6415a41b77ba
+>>>>>>> parent of 0f87603 (error scv)
